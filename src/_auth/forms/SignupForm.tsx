0c256ea926b1fd37aa247958fcecee8b90649b9f
Dev Jobalia <webdev.jobalia@gmail.com>
2024-01-07 03:07:10 +0530

    IMPLEMENT SHADCN 🖼️

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
 
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
 };
 
 export default SignupForm;
