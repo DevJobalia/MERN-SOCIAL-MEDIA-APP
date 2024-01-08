383c128308595768b734b4ea212cd5bc0619e831
Dev Jobalia <webdev.jobalia@gmail.com>
2024-01-08 09:55:23 +0530

    CREATE BOTTOM BAR

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -1,7 +1,35 @@
-import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import { bottombarLinks } from "@/contstants";
 
 const Bottombar = () => {
-  return <div>Bottombar</div>;
+  const { pathname } = useLocation();
+  return (
+    <section className="bottom-bar">
+      {/* <ul className="flex flex-col gap-6"> */}
+      {bottombarLinks.map((link) => {
+        const isActive = pathname === link.route;
+        return (
+          <Link
+            to={link.route}
+            key={link.label}
+            className={`${
+              isActive && "bg-primary-500 rounded-[10px]"
+            } flex-center flex-col gap-1 p-2 transition`}
+          >
+            <img
+              src={link.imgURL}
+              alt={link.label}
+              width={16}
+              height={16}
+              className={` ${isActive && "invert-white"}`}
+            />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Bottombar;
